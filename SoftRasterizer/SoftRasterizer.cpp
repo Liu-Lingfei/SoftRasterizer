a62f6987d809e064cabc9ef5437c90ca6876597f
liulingfei <56315716@qq.com>
2022-08-18 23:29:36 +0800

    完成图像显示，并优化显示速度。 添加Eigen库。

--- SoftRasterizer/SoftRasterizer.cpp
+++ SoftRasterizer/SoftRasterizer.cpp
@@ -4,7 +4,51 @@ SoftRasterizer::SoftRasterizer(QWidget *parent)
     : QMainWindow(parent)
 {
     ui.setupUi(this);
+
+    resize(1000, 1000);
+
+    img = QImage(imageWidth, imageHeight, QImage::Format_RGBA8888);
+    data = img.bits();
+    img.fill(Qt::white);
+
+    label.setParent(this);
+    label.setGeometry(0, 100, img.width(), img.height());
+    label.show();
 }
 
 SoftRasterizer::~SoftRasterizer()
-{}
+{
+}
+
+void SoftRasterizer::paintEvent(QPaintEvent*)
+{
+    clock_t start, end;
+
+    start = clock();
+    int pixIndex = 0;
+	for (int i = 0; i < imageWidth; ++i) {
+		for (int j = 0; j < imageHeight; ++j) {
+			data[pixIndex + 0] = 128;
+			data[pixIndex + 1] = 255;
+			data[pixIndex + 2] = 255;
+			data[pixIndex + 3] = 255;
+			pixIndex += 4;
+		}
+	}
+	end = clock();
+	label.setPixmap(QPixmap::fromImage(img));
+
+    qDebug() << "time = " << double(end - start) / CLOCKS_PER_SEC << "s" << endl;
+
+    testEigen();
+}
+
+void SoftRasterizer::testEigen()
+{
+    MatrixXd m(2, 2);
+    m(0, 0) = 3;
+    m(1, 0) = 2.5;
+    m(0, 1) = -1;
+    m(1, 1) = m(1, 0) + m(0, 1);
+    qDebug() << m(0, 0) << m(1, 0) << m(0, 1) << m(1, 1) << endl;
+}
--- SoftRasterizer/SoftRasterizer.h
+++ SoftRasterizer/SoftRasterizer.h
@@ -3,6 +3,22 @@
 #include <QtWidgets/QMainWindow>
 #include "ui_SoftRasterizer.h"
 
+#include "qpainter.h"
+#include "qimage.h"
+#include "qpixmap.h"
+#include "qdebug.h"
+#include "qlabel.h"
+
+#include <ctime>
+#include <cstdlib>
+#include <string>
+#include <sstream>
+#include <fstream>
+#include <iostream>
+
+#include "Eigen/Dense"
+using Eigen::MatrixXd;
+
 class SoftRasterizer : public QMainWindow
 {
     Q_OBJECT
@@ -11,6 +27,19 @@ public:
     SoftRasterizer(QWidget *parent = nullptr);
     ~SoftRasterizer();
 
+protected:
+    void paintEvent(QPaintEvent*);
+
 private:
+    void testEigen();
+
     Ui::SoftRasterizerClass ui;
+
+    const int imageWidth = 1000;
+    const int imageHeight = 800;
+
+    QPixmap pix;
+    QImage img;
+    QLabel label;
+    uchar* data;
 };
